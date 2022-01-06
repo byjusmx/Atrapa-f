4aadc89ada73868b2b377787952c55283f6fa3f2
byjusmx <87218747+byjusmx@users.noreply.github.com>
2022-01-06 07:01:15 -0600

    Update form.js

--- js/form.js
+++ js/form.js
@@ -14,7 +14,7 @@ class Form{
     }
     display() {
         this.title.html("ATRAPA FRUTAS");
-        this.title.position(350, 50);
+        this.title.position(350, 150);
         this.title.style('font-size', '70px');
         this.title.style('color', 'skyblue');
         this.input.position(550,400);
